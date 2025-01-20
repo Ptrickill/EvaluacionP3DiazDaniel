fcf65adef3c4a28364055273ea0c30e29fa7af53
ptrickill <daniel.diaz.mosquera8@gmail.com>
2025-01-20 15:52:31 -0500

    agregacion de carpetas y creacion de archivos .cs

---
+++ Data/SQLiteHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace EvaluacionP3DiazDaniel.Data
+{
+    public class SQLiteHelper
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public SQLiteHelper(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Country>().Wait();
+        }
+
+        public Task<int> SaveCountryAsync(Country country)
+        {
+            return _database.InsertAsync(country);
+        }
+
+        public Task<List<Country>> GetCountriesAsync()
+        {
+            return _database.Table<Country>().ToListAsync();
+        }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace EvaluacionP3DiazDaniel.Models
+{
+    public class Country
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public string GoogleMapsLink { get; set; }  
+        public string CustomName { get; set; }
+    }
+}
