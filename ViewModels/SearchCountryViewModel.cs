88ccf8384465b9bbdb0e24cf566a123160e096fe
ptrickill <daniel.diaz.mosquera8@gmail.com>
2025-01-20 17:48:32 -0500

    view model

--- ViewModels/SearchCountryViewModel.cs
+++ ViewModels/SearchCountryViewModel.cs
@@ -12,11 +12,64 @@ using System.Windows.Input;
 
 namespace EvaluacionP3DiazDaniel.ViewModels
 {
-    public class SearchCountryViewModel
+    public class SearchCountryViewModel : ObservableObject
     {
         private readonly RestCountriesService _restCountriesService;
         private readonly SQLiteHelper _database;
 
-        public
+        public SearchCountryViewModel(RestCountriesService restCountriesService, SQLiteHelper database)
+        {
+            _restCountriesService = restCountriesService;
+            _database = database;
+
+            SearchCommand = new RelayCommand(async () => await SearchCountryAsync());
+            ClearCommand = new RelayCommand(ClearSearch);
+        }
+
+        private string _countryName;
+        public string CountryName
+        {
+            get => _countryName;
+            set => SetProperty(ref _countryName, value);
+        }
+
+        private string _resultMessage;
+        public string ResultMessage
+        {
+            get => _resultMessage;
+            set => SetProperty(ref _resultMessage, value);
+        }
+
+        public ICommand SearchCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        private async Task SearchCountryAsync()
+        {
+            if (string.IsNullOrWhiteSpace(CountryName))
+            {
+                ResultMessage = "Por favor, ingrese un nombre de pais";
+                return;
+            }
+
+            var country = await _restCountriesService.GetCountryByNameAsync(CountryName);
+
+            if (country == null)
+            {
+                country.CustomName = "DDiaz";
+                await _database.SaveCountryAsync(country);
+
+                ResultMessage = $"Pais encontrado: {country.Name}, region: {country.Region}. Guardado en la base de datos"; 
+            }
+            else
+            {
+                ResultMessage = "No se encontro ningun pais con ese nombre";
+            }
+        }
+
+        private void ClearSearch()
+        {
+            CountryName = string.Empty;
+            ResultMessage = string.Empty;
+        }
     }
 }
