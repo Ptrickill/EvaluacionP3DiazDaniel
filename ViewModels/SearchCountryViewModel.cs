5f93e5cf29713e2faea1f28c4cd1f627e14ec780
ptrickill <daniel.diaz.mosquera8@gmail.com>
2025-01-20 16:38:08 -0500

    primer viewModel

---
+++ ViewModels/SearchCountryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EvaluacionP3DiazDaniel.Data;
+using EvaluacionP3DiazDaniel.Models;
+using EvaluacionP3DiazDaniel.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace EvaluacionP3DiazDaniel.ViewModels
+{
+    public class SearchCountryViewModel
+    {
+        private readonly RestCountriesService _restCountriesService;
+        private readonly SQLiteHelper _database;
+
+        public
+    }
+}
